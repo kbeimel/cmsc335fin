0087c54c00fbe3c7a696d04a9540e3f3c710bcc3
kbeimel <kbeimel@terpmail.umd.edu>
2025-12-14 01:25:27 -0500

    Add files via upload

---
+++ finalserver.js
@@ -0,0 +1,153 @@
+const path = require('path');
+const express = require('express');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const API_BASE = 'https://api.artic.edu/api/v1/artworks/search';
+
+
+require("dotenv").config({
+    path: path.resolve(__dirname, "credentialsDontPost/.env"),
+});
+
+const def_port = 4000;
+const u_port = parseInt(process.argv[2]);
+const port = u_port || def_port;
+
+const httpSuccessStatus = 200;
+
+const databaseName = "CMSC335DB";
+const collectionName = "artHistory";
+const uri = process.env.MONGO_CONNECTION_STRING;
+
+app.get('/', (req, res) => { res.redirect('/homepage.shtml'); });
+
+app.use(express.static(__dirname));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+app.post('/make-search', async (req, res) => { //was submit-application
+
+    const client = new MongoClient(uri, { serverApi: ServerApiVersion.v1 });
+    const query = req.body.q;
+
+    if (!query) {
+        return res.status(400).send("missing search query");
+    }
+
+    let work = null;
+
+    try {
+        const apiURL = `${API_BASE}?q=${query}&fields=id,title,artist_display,&limit=1`;
+        await client.connect();
+        const database = client.db(databaseName);
+        const collection = database.collection(collectionName);
+
+
+        const apiResponse = await axios.get(API_BASE);
+        result = await collection.insertOne(query);
+
+        const { data, config } = apiResponse.data;
+
+        if (data.length === 0) {
+            return res.status(400).send("no results found. please try another search term.");
+        }
+
+        const artwork = data[0];
+        const baseURL = config.iiif_url;
+        let imageURL = null;
+
+        if (artwork.image_id) {
+            imageURL = `${baseURL}/${artwork.image_id}/full/843,/0/default.jpg`
+        }
+
+        work = {
+            id: artwork.id,
+            title: artwork.title,
+            artist: artwork.artist_display,
+            imageUrl: imageURL,
+        };
+
+
+        res.send(`<!DOCTYPE html>
+                <html lang="en">
+                    <head>
+                    <link rel="stylesheet" href="style.css">  
+                    <meta charset="utf-8" > 
+                    <title>Search</title>	
+                    </head>
+                    <h1>${work.title}</h1>
+                    <strong>Artist:</strong> ${work.artist} <br>
+                    <img src=${imageURL}/>
+                    <br>
+                    <a href="homepage.shtml">back to home</a>
+`);
+
+    } catch (e) {
+        console.error(e);
+        res.end();
+    } finally {
+        await client.close();
+    }
+
+}
+)
+
+
+
+app.get('/show-history', async (req, res) => {
+
+    try {
+        await client.connect();
+        const collection = client.db(databaseName).collection(collectionName);
+
+        const filter = {};
+        cursor = collection.find(filter);
+        result = await cursor.toArray();
+        let answer = "";
+        result.forEach(elem => answer += `${elem} <br>`);
+        answer += `Found: ${result.length} queries`;
+        res.send(answer);
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+
+});
+
+
+app.post('/clear-history', async (req, res) => {
+    const client = new MongoClient(uri, { serverApi: ServerApiVersion.v1 });
+
+    try {
+        await client.connect();
+        const database = client.db(databaseName);
+        const collection = database.collection(collectionName);
+        const numm = await collection.estimatedDocumentCount();
+        const result = await collection.drop();
+        res.send(`
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <link rel="stylesheet" href="style.css">  
+    <meta charset="utf-8" > 
+	<title>Clear History</title>	
+	</head>
+    <h1>success.</h1>
+    ${numm} queries were removed.
+    <hr>
+    <a href="homepage.shtml">back to home</a>`);
+    } catch (e) {
+        res.send(e);
+    } finally {
+        await client.close();
+    }
+});
+
+
+app.listen(port, () => {
+    console.log(`Web server is running at http://localhost:${port}`);
+});
